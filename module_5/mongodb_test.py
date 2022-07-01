4430f77bf3d47940a0359fb5cd4cea13f3f3d766
Thomas Eagle <tgeagle@gmail.com>
2022-07-01 09:54:28 -0500

    updating mongodb_test

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,25 @@
+""" 
+    Title: mongodb_test.py
+    Author: Thomas Eagle
+    Date: July 1 2022
+    Description: Test program for connecting to a 
+                 MongoDB Atlas cluster
+"""
+""" import statements """
+from pymongo import MongoClient
+
+# MongoDB connection string
+url = "mongodb+srv://admin:admin@cluster0.ohpdktx.mongodb.net/?retryWrites=true&w=majority"
+
+# connect to the MongoDB cluster
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+# show the connected collections
+print("\n -- Pytech COllection List --")
+print(db.list_collection_names())
+
+# show an exit message
+input("\n\n  End of program, press any key to exit... ")
